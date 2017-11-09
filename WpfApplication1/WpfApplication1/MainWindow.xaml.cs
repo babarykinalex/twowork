0d6775637365b6c29dc45aaf62158be49123fbe6
user <user@USER-PC>
2017-11-09 23:01:47 +0700

    edit

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace WpfApplication1
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("ok");
+            MessageBox.Show("edit");
         }
     }
 }

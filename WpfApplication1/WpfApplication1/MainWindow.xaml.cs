0f39e24b314494f8b7013994166c85d1267a2f2f
Erzhanat <Mc-er008@mail.ru>
2017-11-10 11:07:00 +0700

    calcul

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -20,6 +20,10 @@ namespace WpfApplication1
     /// </summary>
     public partial class MainWindow : Window
     {
+        int a = 0;
+        int b = 0;
+        int z = 0;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -29,6 +33,194 @@ namespace WpfApplication1
         {
             MessageBox.Show("Hello");
         }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            textbox1.Text = "0";
+            textbox1.Focus();
+            a = 0; 
+            b = 0;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button2_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button6_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void button7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button14_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button15_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button16_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button17_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+     
+        private void button18_Click(object sender, RoutedEventArgs e)
+        {
+       
+        }
+
+        private void button19_Click(object sender, RoutedEventArgs e)
+        {
+            a = Convert.ToInt32(textbox1.Text);
+            textbox1.Text = Convert.ToString(Math.Sqrt(a));
+        }
+
+        private void button20_Click(object sender, RoutedEventArgs e)
+        {
+            a = Convert.ToInt32(textbox1.Text);
+            z = 20;
+            textbox1.Text = "0";
+        }
+
+        private void button21_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button22_Click(object sender, RoutedEventArgs e)
+        {
+            a = Convert.ToInt32(textbox1.Text);
+            z = 22;
+            textbox1.Text = "0";
+        }
+
+        private void button23_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button24_Click(object sender, RoutedEventArgs e)
+        {
+            a = Convert.ToInt32(textbox1.Text);
+            z = 24;
+            textbox1.Text = "0";
+        }
+
+        private void button25_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button26_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button27_Click(object sender, RoutedEventArgs e)
+        {
+            a = Convert.ToInt32(textbox1.Text);
+            z = 27;
+            textbox1.Text = "0";
+        }
+
+        private void button28_Click(object sender, RoutedEventArgs e)
+        {
+            b = Convert.ToInt32(textbox1.Text);
+            if (z != 0)
+            {
+                if (z == 27)
+                {
+                    textbox1.Text = Convert.ToString(a + b);
+                    z = 0;
+                }
+                if (z == 24)
+                {
+                    textbox1.Text = Convert.ToString(a - b);
+                    z = 0;
+                }
+                if(z==22)
+                {
+                    textbox1.Text = Convert.ToString(a * b);
+                    z = 0;
+                }
+                if (z == 20)
+                {
+                    textbox1.Text = Convert.ToString(a / b);
+                    z = 0;
+                }
+            }
+        }
+
+      
 
 
     }
